55629ed5ac0f58b108475d69c247ff6a728d8b49
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 12:25:25 +0530

    basics of JAVA programming.

---
+++ 1-VIDEO/JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics {
+
+    public static void main(String args[]){
+        System.out.println("Hello Rahul");
+        System.out.println("Hello Rahul"); //ln means new line (after the complete line it will break to new line)
+        System.out.print("Hello Rahul \n"); // \n  (it will break line where \n is called)
+    }
+}
