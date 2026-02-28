85ad3a7f48844248cd62c07ac38d87f83b4501d4
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 18:38:05 +0530

    # != operator

--- 2-Operators/TypesOfOperators.java
+++ 2-Operators/TypesOfOperators.java
@@ -15,6 +15,7 @@ public class TypesOfOperators {
 
         //Relational (==,!= , > , < , >= , <=)
         System.out.println(a == b);
+        System.out.println(a != b);
 
         //Logical
     }
