2249a0d34309e2835b157ca8ac4f686838a380df
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 15:00:17 +0530

    #take input from user

---
+++ 1-Basics/InputInJava.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class InputInJava {
+
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        String input = sc.next();//store
+        System.out.println(input);
+    }
+}
