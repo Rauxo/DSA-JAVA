21956fb51ac17e8f335da894fb7e487ab9cd4f9f
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 15:43:12 +0530

    #type casting

--- 1-Basics/TypeConversion.java
+++ 1-Basics/TypeConversion.java
@@ -1,9 +1,14 @@
 public class TypeConversion {
 
     public static void main(String args[]){
-        int a = 25;
-        long b = a;
-        System.out.println(b);
+        // int a = 25;
+        // long b = a;
+        // System.out.println(b);
+
+        //type casting (forcefully change the type)
+        float c = 25.99f;
+        int d = (int)c; // lossy conversion
+        System.out.println(d);
     }
 }
 
