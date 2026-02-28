896141dc7fdcbc2b4121e80f1b29224adf70f606
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 15:05:54 +0530

    input float

--- 1-Basics/InputInJava.java
+++ 1-Basics/InputInJava.java
@@ -4,14 +4,27 @@ public class InputInJava {
 
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
+
+        //only for one word
+
         // String input = sc.next();// store
         // System.out.println(input);
 
 
+        //Multiple Word
+
         // String FullName = sc.nextLine();
         // System.out.println(FullName);
 
-        int number = sc.nextInt();
-        System.out.println(number);
+
+        //Input Number 
+
+        // int number = sc.nextInt();
+        // System.out.println(number);
+
+        //Input Floating Number
+
+        float fl = sc.nextFloat();
+        System.out.println(fl);
     }
 }
