1391082078cceb890d44da922b598c62d6134dc3
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 12:42:31 +0530

    initialize variables and print

---
+++ 1-Basics/Variables.java
@@ -0,0 +1,16 @@
+public class Variables {
+
+    public static void main(String args[]){
+        int a  = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Rahul Saikia";
+        System.out.println(name);
+
+        a = 50;
+        System.out.println(a);
+        b = a;
+        System.out.println(b);
+    }
+}
