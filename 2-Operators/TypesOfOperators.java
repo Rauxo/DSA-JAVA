a2faf9ea19903cfe4ddeec5ef55460cdc1e0a870
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 18:33:53 +0530

    #Arithmatic operators

---
+++ 2-Operators/TypesOfOperators.java
@@ -0,0 +1,21 @@
+public class TypesOfOperators {
+
+    public static void main(String args[]){
+        //Arithmetic Binary (+,-,/,*,%) and unary (++,--)
+        int a = 10 ;
+        int b = 2;
+        System.out.println(a+b);
+        System.out.println(a/b);
+        System.out.println(a*b);
+        System.out.println(a-b);
+        System.out.println(a%b);
+
+        int c = a++;
+        System.out.println(c);
+
+        //Relational
+
+
+        //Logical
+    }
+}
