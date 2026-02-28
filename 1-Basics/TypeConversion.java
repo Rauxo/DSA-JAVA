9ad286b6745e2b167698c2fc2a06986053ba9b1d
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 15:34:11 +0530

    #type conversion

---
+++ 1-Basics/TypeConversion.java
@@ -0,0 +1,10 @@
+public class TypeConversion {
+
+    public static void main(String args[]){
+        int a = 25;
+        long b = a;
+        System.out.println(b);
+    }
+}
+
+// coversion  can done in :- byte -> short -> int -> float -> long -> double
