626418239db490b461a0ea68af1c23bd307ab6e0
Rauxo <rahulsaikia682@gmail.com>
2026-02-28 12:55:11 +0530

    data types in JAVA

---
+++ 1-Basics/DataTypes.java
@@ -0,0 +1,15 @@
+public class DataTypes {
+
+    public static void main(String args[]){
+        byte b = 8; 
+        System.out.println(b);
+        char ch = 'a'; 
+        System.out.println(ch);
+        boolean bool = true; 
+        System.out.println(bool);
+        float fl = 10.5f;
+        System.out.println(fl);
+        int number = 20;
+        System.out.println(number);
+    }
+}
