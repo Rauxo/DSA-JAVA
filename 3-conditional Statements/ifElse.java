92d8115f454bde0f4ac8d51fceefa677852c50a5
Rauxo <rahulsaikia682@gmail.com>
2026-03-03 12:58:09 +0530

    #conditional statement (if else)

---
+++ 3-conditional Statements/ifElse.java
@@ -0,0 +1,11 @@
+public class ifElse {
+
+    public static void main(String args[]){
+        int a = 10;
+        if(a == 10){
+            System.out.println("The Number is equal to 10");
+        }else{
+            System.out.println("The Number is Not equal to 10");
+        }
+    }
+}
