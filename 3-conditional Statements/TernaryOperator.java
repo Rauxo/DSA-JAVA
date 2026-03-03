e77902087150c4e133cef6e9bd133994f8a28e4d
Rauxo <rahulsaikia682@gmail.com>
2026-03-03 13:52:03 +0530

    #TernaryOperator Odd Even

--- 3-conditional Statements/TernaryOperator.java
+++ 3-conditional Statements/TernaryOperator.java
@@ -3,8 +3,13 @@ public class TernaryOperator {
     public static void main(String args[]){
         // variable = condition ? statement 1 : statement 2;
 
-        int a = 10;
-        int result = a == 10 ? a = ++a : a;
-        System.out.println(result);
+        // int a = 10;
+        // int result = a == 10 ? a = ++a : a;
+        // System.out.println(result);
+
+        int number = 11;
+        String type = ((number%2) == 0) ? "Even" : "Odd";
+        System.out.println(type);
+
     }
 }
