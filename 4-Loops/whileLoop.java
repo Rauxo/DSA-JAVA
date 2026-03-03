12e739d636e40c43145f05897ea1ad8ec1bc2480
Rauxo <rahulsaikia682@gmail.com>
2026-03-03 17:32:23 +0530

    #while loop

---
+++ 4-Loops/whileLoop.java
@@ -0,0 +1,10 @@
+public class whileLoop {
+
+    public static void main(String args[]){
+        int count = 0;
+        while(count < 10){
+            System.out.println("Hello World");
+            count ++;
+        }
+    }
+}
