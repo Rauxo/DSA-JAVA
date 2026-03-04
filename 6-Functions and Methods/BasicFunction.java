b1b43e032614bd610038b055b4f9b5cb9697d5a9
Rauxo <rahulsaikia682@gmail.com>
2026-03-04 17:27:09 +0530

    #introduction of Function in JAVA

---
+++ 6-Functions and Methods/BasicFunction.java
@@ -0,0 +1,11 @@
+public class BasicFunction {
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+    public static void main(String args[]){
+        printHelloWorld();
+        printHelloWorld();
+        printHelloWorld();
+    }
+}
