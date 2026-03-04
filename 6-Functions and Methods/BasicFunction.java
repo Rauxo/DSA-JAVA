714d7de0b7e3e52c2834f6e2a39f10b4ff7282d4
Rauxo <rahulsaikia682@gmail.com>
2026-03-04 17:29:06 +0530

    #return

--- 6-Functions and Methods/BasicFunction.java
+++ 6-Functions and Methods/BasicFunction.java
@@ -2,6 +2,7 @@ public class BasicFunction {
 
     public static void printHelloWorld(){
         System.out.println("Hello World");
+        return;
     }
     public static void main(String args[]){
         printHelloWorld();
