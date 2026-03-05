14f7d9abef40dfb3b067ef181a96c210298b4517
Rauxo <rahulsaikia682@gmail.com>
2026-03-05 15:04:58 +0530

    #format document

--- 6-Functions and Methods/CheckNumberPrimeOrNot.java
+++ 6-Functions and Methods/CheckNumberPrimeOrNot.java
@@ -1,20 +1,22 @@
 import java.util.*;
+
 public class CheckNumberPrimeOrNot {
 
-    public static boolean isPrime(int n){
-        //corner case
-        if(n == 2){
+    public static boolean isPrime(int n) {
+        // corner case
+        if (n == 2) {
             return true;
         }
         boolean isPrime = true;
-        for(int i = 2; i<=Math.sqrt(n) ; i++){
-            if(n % i == 0){
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
                 isPrime = false;
             }
         }
         return isPrime;
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         System.out.println(isPrime(3));
     }
 }
