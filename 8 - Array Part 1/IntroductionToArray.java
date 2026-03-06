e47e660015cff7b1b178aa7dbcb07f8625848062
Rauxo <rahulsaikia682@gmail.com>
2026-03-06 18:39:00 +0530

    # input and output in an Array

--- 8 - Array Part 1/IntroductionToArray.java
+++ 8 - Array Part 1/IntroductionToArray.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class IntroductionToArray {
     public static void main(String args[]) {
         // create : dataType arrayName[] = new dataType[size]
@@ -5,5 +6,16 @@ public class IntroductionToArray {
 
         int numbers[] = {1,2,3} ;
         String fruits[] = {"apple","mango"};
+
+        //input in an Array
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt(); 
+        marks[1] = sc.nextInt();
+        marks[2] = sc.nextInt();
+
+        //output
+        System.out.println("Physics : " + marks[0]);
+        System.out.println("Math : " + marks[1]);
+        System.out.println("Chem : " + marks[2]);
     }
 }
