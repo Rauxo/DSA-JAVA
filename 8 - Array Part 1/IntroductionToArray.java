a97bfd5d7a8803f4409e865cb5f1b650383e00fe
Rauxo <rahulsaikia682@gmail.com>
2026-03-06 18:43:25 +0530

    #find percentage

--- 8 - Array Part 1/IntroductionToArray.java
+++ 8 - Array Part 1/IntroductionToArray.java
@@ -24,5 +24,8 @@ public class IntroductionToArray {
         System.out.println("Math : " + marks[1]);
         marks[1] = marks[1] + 1;
         System.out.println("Math : " + marks[1]);
+
+        int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        System.out.println("Percentage is :- " + percentage);
     }
 }
