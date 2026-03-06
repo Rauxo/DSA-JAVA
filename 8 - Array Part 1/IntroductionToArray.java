b81c3668e56ca0c9aac7b779dc2f6ba04ce1d3bd
Rauxo <rahulsaikia682@gmail.com>
2026-03-06 18:33:21 +0530

    #creation of an Array

---
+++ 8 - Array Part 1/IntroductionToArray.java
@@ -0,0 +1,9 @@
+public class IntroductionToArray {
+    public static void main(String args[]) {
+        // create : dataType arrayName[] = new dataType[size]
+        int marks[] = new int[50];
+
+        int numbers[] = {1,2,3} ;
+        String fruits[] = {"apple","mango"};
+    }
+}
