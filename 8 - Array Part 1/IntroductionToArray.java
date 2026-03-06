4b82061cbc1796b05385c7e3d531bded9e0be692
Rauxo <rahulsaikia682@gmail.com>
2026-03-06 18:40:53 +0530

    #update in an Array

--- 8 - Array Part 1/IntroductionToArray.java
+++ 8 - Array Part 1/IntroductionToArray.java
@@ -1,21 +1,26 @@
 import java.util.*;
+
 public class IntroductionToArray {
     public static void main(String args[]) {
         // create : dataType arrayName[] = new dataType[size]
         int marks[] = new int[50];
 
-        int numbers[] = {1,2,3} ;
-        String fruits[] = {"apple","mango"};
+        int numbers[] = { 1, 2, 3 };
+        String fruits[] = { "apple", "mango" };
 
-        //input in an Array
+        // input in an Array
         Scanner sc = new Scanner(System.in);
-        marks[0] = sc.nextInt(); 
+        marks[0] = sc.nextInt();
         marks[1] = sc.nextInt();
         marks[2] = sc.nextInt();
 
-        //output
+        // output
         System.out.println("Physics : " + marks[0]);
         System.out.println("Math : " + marks[1]);
         System.out.println("Chem : " + marks[2]);
+
+        //update
+        marks[1] = 100;
+        System.out.println("Math : " + marks[1]);
     }
 }
