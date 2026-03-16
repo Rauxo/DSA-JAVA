556f292baee74d05650fd1a7299d6c97a1fd43b6
Rauxo <rahulsaikia682@gmail.com>
2026-03-16 14:38:37 +0530

    #best time to buy and sell stock

---
+++ 9-Array Part 2/BuyAndSellStock.java
@@ -0,0 +1,21 @@
+public class BuyAndSellStock {
+    public static int buyAndSellStocks(int price[]) {
+        int buyPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < price.length; i++) {
+            if (buyPrice < price[i]) {
+                int profit = price[i] - buyPrice; // today's profit
+                maxProfit = Math.max(maxProfit, profit); // global profit
+            } else {
+                buyPrice = price[i];
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String args[]) {
+        int prices[] = { 7, 1, 5, 3, 6, 4 };
+        System.out.println(buyAndSellStocks(prices));
+    }
+}
